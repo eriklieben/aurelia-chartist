9acda032c896696bd1c5f35e337a3eb0387a4cf0
Erik Lieben <erik.lieben@effectory.com>
2016-09-10 13:50:55 +0200

    chore: initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+System.config({
+  defaultJSExtensions: true,
+  transpiler: false,
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0",
+    "chartist": "npm:chartist@0.9.8",
+    "npm:aurelia-polyfills@1.0.0": {
+      "aurelia-pal": "npm:aurelia-pal@1.0.0"
+    }
+  }
+});
